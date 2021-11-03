09dbf0cdfb7e08df53d617940434039249db9c76
Yago Ascasibar <yago.ascasibar@uam.es>
2021-11-03 17:55:18 +0100

    More fixes

--- pst/fit.py
+++ pst/fit.py
@@ -10,10 +10,10 @@ import pst
 
 class Polynomial_MFH_fit:
 
-    def __init__(self, N, SSP, obs_filters, t_obs, t_end=t_obs, t_start=0):
-        self.t_start = t_start
-        self.t_end = t_end
+    def __init__(self, N, SSP, obs_filters, t_obs, **kwargs):
         self.t_obs = t_obs
+        self.t_start = kwargs.get('t_start', 0*u.Gyr)
+        self.t_end = kwargs.get('t_end', t_obs)
         primordial_coeffs = []
         primordial_L = []
         for n in range(N):
@@ -24,7 +24,8 @@ class Polynomial_MFH_fit:
             primordial_coeffs.append(c)
 
             L = []
-            p = pst.models.Polynomial_MFH(t_start=t_start, t_end=t_end, coeffs=c)
+            p = pst.models.Polynomial_MFH(
+                t_start=self.t_start, t_end=self.t_end, coeffs=c)
             sed = p.compute_SED(SSP, t_obs)
             for filter_name in obs_filters:
                 photo = pst.observables.luminosity(
