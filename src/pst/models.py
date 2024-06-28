6a87bfde4b9fbabc0e001d1a8d588b129c53795a
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-06-28 21:37:13 +0200

    add compute photometry method

--- src/pst/models.py
+++ src/pst/models.py
@@ -8,6 +8,7 @@ import pst
 from scipy import interpolate
 from abc import ABC, abstractmethod
 
+
 class ChemicalEvolutionModel(ABC):
     """TODO
 
@@ -25,10 +26,9 @@ class ChemicalEvolutionModel(ABC):
         self.M_gas = kwargs.get('M_gas', 0*u.Msun)
         self.Z = kwargs.get('Z', 0.02)
 
-    def compute_SED(self, SSP : pst.SSP.SSPBase, t_obs : u.Quantity,
-                    allow_negative=True):
-        """Compute the SED of a given model observed at a given time.
-        
+    def interpolate_ssp_masses(self, SSP: pst.SSP.SSPBase, t_obs: u.Quantity):
+        """Interpolate the star formation history to compute the SSP stellar massess.
+
         Description
         -----------
         This method computes the spectra energy distribution resulting from the
@@ -41,14 +41,11 @@ class ChemicalEvolutionModel(ABC):
         - t_obs: astropy.Quantity
             Cosmic time at which the galaxy is observed. This will prevent the
             use the SSP with ages older than `t_obs`.
-        - allow_negative: bool, default=True
-            Allow for some SSPs to have negative masses during the computation
-            of the resulting SED.
-        
+
         Returns
         -------
-        - sed: astropy.Quantity
-            Spectral energy distribution in the same units as `SSP.L_lambda`.
+        - masses: astropy.Quantity
+            Corresponding stellar mass of each SSP.
         """
         age_bins = np.hstack(
             [0 << u.yr, np.sqrt(SSP.ages[1:] * SSP.ages[:-1]), 1e12 << u.yr])
@@ -75,6 +72,34 @@ class ChemicalEvolutionModel(ABC):
         weights[z_indices - 1, t_indices] = 1 - weight_Z
         weights[:, t_indices] = weights[:, t_indices] * M_bin[np.newaxis, :]
         weights = weights << u.Msun
+        return weights
+        
+    def compute_SED(self, SSP : pst.SSP.SSPBase, t_obs : u.Quantity,
+                    allow_negative=True):
+        """Compute the SED of a given model observed at a given time.
+        
+        Description
+        -----------
+        This method computes the spectra energy distribution resulting from the
+        chemical evolution model observed at a given time.
+
+        Parameters
+        ----------
+        - SSP: pst.SSP.SSP
+            The SSP model to used for synthezising the SED.
+        - t_obs: astropy.Quantity
+            Cosmic time at which the galaxy is observed. This will prevent the
+            use the SSP with ages older than `t_obs`.
+        - allow_negative: bool, default=True
+            Allow for some SSPs to have negative masses during the computation
+            of the resulting SED.
+        
+        Returns
+        -------
+        - sed: astropy.Quantity
+            Spectral energy distribution in the same units as `SSP.L_lambda`.
+        """
+        weights = self.interpolate_ssp_masses(SSP, t_obs)
         if not allow_negative:
             mask = (weights > 0) & np.isfinite(weights)
         else:
@@ -82,6 +107,17 @@ class ChemicalEvolutionModel(ABC):
         sed = np.sum(weights[mask, np.newaxis] * SSP.L_lambda[mask, :],
                     axis=(0))
         return sed
+
+    def compute_photometry(self, ssp, t_obs, photometry=None):
+        weights = self.interpolate_ssp_masses(ssp, t_obs)
+        extra_dim = photometry.ndim - weights.ndim
+        if extra_dim > 0:
+            new_dims = tuple(np.arange(extra_dim))
+            np.expand_dims(weights, new_dims)
+        if photometry is None:
+            photometry = ssp.photometry
+        model_photometry = np.sum(photometry * weights, axis=(-1, -2))
+        return model_photometry
 
     @abstractmethod
     def integral_SFR(self):
