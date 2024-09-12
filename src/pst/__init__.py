da7c90e1fa5dbb3732450fd74eebf0f360dc4393
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-09-12 15:11:19 +0200

    add version

--- src/pst/__init__.py
+++ src/pst/__init__.py
@@ -2,4 +2,7 @@ from . import SSP
 from . import observables
 from . import models
 from . import fitting_module
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
 __all__ = [SSP, observables, models, fitting_module]
