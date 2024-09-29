9ea979bf36699d9157125a964fe41f877159c670
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-09-29 11:09:42 +0200

    add documentation

--- src/pst/utils.py
+++ src/pst/utils.py
@@ -1,20 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Sun Jun  5 16:18:39 2022
-
-@author: pablo
+Module containing generic utility functions
 """
 
 import numpy as np
-from matplotlib import pyplot as plt
-
 from astropy import units as u
+
 
 SQRT_2 = np.sqrt(2)
 
 def flux_conserving_interpolation(new_wave, wave, spectra):
-    """Interpolate a spectra to a new grid of wavelengths preserving the flux density."""
+    """Interpolate a spectra to a new grid of wavelengths preserving the flux density.
+    
+    Parameters
+    ----------
+    new_wave : np.ndarray
+        New grid of wavelengths
+    wave : np.ndarray
+        Original grid of wavelengths
+    spectra : np.ndarray
+        Spectra associated to `wave`.
+    
+    Returns
+    -------
+    interp_spectra : np.ndarray
+        Interpolated spectra to `new_wave`
+    """
     wave_limits = 1.5 * wave[[0, -1]] - 0.5 * wave[[1, -2]]
     wave_edges = np.hstack([wave_limits[0], (wave[1:] + wave[:-1])/2, wave_limits[1]])
 
@@ -31,9 +41,12 @@ def gaussian1d_conv(f, sigma, deltax):
 
     params
     ------
-    - f: (array) 1D array containing the data to be convolved with.
-    - sigma (array) 1D array containing the values of sigma at each value of x
-    - deltax: (float) Step size of x in "physical" units.
+    f : np.array
+        1D array containing the data to be convolved with.
+    sigma : np.array
+        1D array containing the values of sigma at each value of x
+    deltax : float
+        Step size of x in "physical" units.
     """
     sigma_pixels = sigma / deltax
     pix_range = np.arange(0, f.size, 1)
@@ -54,8 +67,17 @@ def gaussian1d_conv(f, sigma, deltax):
             f_convolved[pixel] = np.sum(f * g)
     return f_convolved
 
-def check_unit(self, quantity, default_unit=None):
-    """Check the units of an input quantity"""
+def check_unit(quantity, default_unit=None):
+    """Check the units of an input quantity.
+    
+    Parameters
+    ----------
+    quantity : np.ndarray or astropy.units.Quantity
+        Input quantity.
+    default_unit : astropy.units.Quantity, default=None
+        If `quantity` has not units, it corresponds to the unit assigned to it.
+        Otherwise, it is used to check the equivalency with `quantity`.
+    """
     isq = isinstance(quantity, u.Quantity)
     if isq and default_unit is not None:
         if not quantity.is_equivalent(default_unit):
