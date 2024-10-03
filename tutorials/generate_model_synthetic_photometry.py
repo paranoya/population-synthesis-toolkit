ec07c3d1e70ae55d7b034e04b088307c06475fca
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-10-03 11:57:28 +0200

    remove deprecated tests

--- tutorials/generate_model_synthetic_photometry.py
+++ tutorials/generate_model_synthetic_photometry.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Fri Jun 28 20:20:31 2024
+This tutorial shows how to generate a grid of photometric fluxes 
+from a given SSP model.
 
-@author: pcorchoc
+The grid will have four dimensions:
+- Observed redshift
+- Dust attenuation
+- Filter band
+- SSP metallicity
+- SSP age
+
+The SSP photometric grid can then be combined with a given
+star formation history to produce a grid of photometric fluxes
+of for different SFHs.
+
 """
 
 from matplotlib import pyplot as plt
@@ -12,52 +21,64 @@ import numpy as np
 from time import time
 from astropy import units as u
 
-from pst.SSP import PopStar, XSL
+from pst.SSP import PopStar
 from pst.dust import DustScreen
 from pst.observables import Filter, load_photometric_filters
-from pst.models import LogNormal_MFH
+from pst.models import LogNormalCEM
 
 # Prepare the SSP photometry
 filters = load_photometric_filters(
-    ['CFHT_MegaCam.u',
-     'PANSTARRS_PS1.g', 'PANSTARRS_PS1.r', 'PANSTARRS_PS1.i', 'PANSTARRS_PS1.z'])
+    ['PANSTARRS_PS1.g', 'PANSTARRS_PS1.r', 'PANSTARRS_PS1.i', 'PANSTARRS_PS1.z'])
 
 # ssp = PopStar(IMF='cha')
-ssp = XSL(IMF='Kroupa', ISO='P00')
+ssp = PopStar(IMF='cha')
+# Interpolate the SED to a coarser grid of wavelengths
 ssp.interpolate_sed(np.arange(1000, 2e4, 5.0) * u.angstrom)
 
+# Instanciate a dust model based on Cardelli+89
 dust_model = DustScreen("ccm89")
 
+# Create an array of values of A_V 
 a_v_array = np.linspace(0, 2, 20)
 redshift_array = np.linspace(0, 2.5, 30)
 
+# Let's also estimate the required amount of time to build the grid
 tstart = time()
-all_photometry = np.zeros((redshift_array.size, a_v_array.size, len(filters),
+all_photometry = np.zeros(
+    (redshift_array.size, a_v_array.size, len(filters),
                            *ssp.L_lambda.shape[:-1]))
 
 for i, z in enumerate(redshift_array):
     for j, av in enumerate(a_v_array):
+        # For each value of AV, we create a new SSP model
         red_ssp = dust_model.redden_ssp_model(ssp, a_v=av)
-        all_photometry[i, j] = red_ssp.compute_photometry(filter_list=filters, z_obs=z)
+        # Compute the SSP photometry at the observed redhisft z
+        all_photometry[i, j] = red_ssp.compute_photometry(filter_list=filters,
+                                                          z_obs=z)
 
 tend = time()
 print("time spent generating SSP photometry: ", tend - tstart)
-print("time spent generating a single SSP model: ", (tend - tstart) / (all_photometry.shape[0] * all_photometry.shape[1]))
+print("time spent generating a single SSP model photometry: ",
+      (tend - tstart) / (all_photometry.shape[0] * all_photometry.shape[1]))
 
-# %%Compute the SFH model photometry
+# Instanciate a chemical evolution model
+
 lnt_array = np.linspace(np.log(0.1), np.log(10), 10)
 scale_array = np.linspace(0.1, 5, 20)
 
 all_models_photometry = np.zeros(
     (lnt_array.size, scale_array.size, *all_photometry.shape[:-2]))
 
-model = LogNormal_MFH(alpha=0, z_today=0.02, lnt0=3, scale=1, m_today=1e9 * u.Msun)
+model = LogNormalCEM(alpha_powerlaw=0, ism_metallicity_today=0.02, lnt0=3,
+                     scale=1, mass_today=1e9 * u.Msun)
+
+# Benchmark the time spent creating the photometric grid
 tstart = time()
 for i, lnt in enumerate(lnt_array):
     for j, scale in enumerate(scale_array):
         model.lnt0 = lnt
         model.scale = scale
-        masses = model.interpolate_ssp_masses(ssp, t_obs=ssp.ages.max())
+
         all_models_photometry[i, j] = model.compute_photometry(
             ssp, t_obs=ssp.ages.max(), photometry=all_photometry)
 
