f551dde84e8a1d574bb526067e1949eba00a7ded
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2026-05-12 19:58:58 +0200

    add more tests

--- tests/test_ssp.py
+++ tests/test_ssp.py
@@ -18,6 +18,29 @@ def make_dummy_ssp():
     ssp.L_lambda = np.ones((2, 2, 5)) << (u.Lsun / u.AA / u.Msun)
     return ssp
 
+
+def make_dummy_cloudy_ssp(include_gas=False):
+    ssp = SSP.SSPwithCloudyGasModel()
+    ssp.name = "toy_cloudy_ssp"
+    ssp.ages = np.array([1e6, 1e7]) * u.yr
+    ssp.metallicities = np.array([0.01, 0.02]) << u.dimensionless_unscaled
+    ssp.wavelength = np.array([100, 200, 400, 800, 1600]) << u.AA
+    ssp.stellar_L_lambda = np.ones((2, 2, 5)) << (u.Lsun / u.AA / u.Msun)
+
+    if include_gas:
+        ssp.log_u_array = np.array([-3.0, -1.0]) << u.dex(u.dimensionless_unscaled)
+        ssp.gas_L_lambda_grid = np.array([
+            np.ones((2, 2, 5)) * 2.0,
+            np.ones((2, 2, 5)) * 4.0,
+        ]) << (u.Lsun / u.AA / u.Msun)
+        ssp.gas_transmission_grid = np.array([
+            np.ones((2, 2, 5)) * 0.8,
+            np.ones((2, 2, 5)) * 0.6,
+        ]) << u.dimensionless_unscaled
+        ssp.log_u = -2.0 << u.dex(u.dimensionless_unscaled)
+
+    return ssp
+
 class TestSSP(unittest.TestCase):
 
     @classmethod
@@ -78,6 +101,88 @@ class TestSSP(unittest.TestCase):
 
         self.assertTrue(np.allclose(ssp_copy.returned_mass_frac,
                                     np.array([[0.1, 0.2], [0.3, 0.4]])))
+
+    def test_cloudy_mixin_cut_wavelength_without_gas_keeps_sed_consistent(self):
+        ssp = make_dummy_cloudy_ssp(include_gas=False)
+
+        ssp.cut_wavelength(150, 900, verbose=False)
+
+        self.assertEqual(ssp.wavelength.size, 3)
+        self.assertEqual(ssp.stellar_L_lambda.shape[-1], 3)
+        self.assertEqual(ssp.L_lambda.shape[-1], 3)
+
+    def test_cloudy_mixin_regrid_without_gas_keeps_sed_consistent(self):
+        ssp = make_dummy_cloudy_ssp(include_gas=False)
+
+        ssp.regrid(np.array([0.5, 5.0]), np.array([0.01]), verbose=False)
+
+        self.assertEqual(ssp.ages.size, 2)
+        self.assertEqual(ssp.metallicities.size, 1)
+        self.assertEqual(ssp.stellar_L_lambda.shape[:2], (1, 2))
+        self.assertEqual(ssp.L_lambda.shape[:2], (1, 2))
+
+    def test_cloudy_mixin_log_u_clamps_to_grid_edges(self):
+        ssp = make_dummy_cloudy_ssp(include_gas=True)
+
+        ssp.log_u = -4.0 << u.dex(u.dimensionless_unscaled)
+        self.assertTrue(np.allclose(
+            ssp.gas_L_lambda.value,
+            ssp.gas_L_lambda_grid[0].value))
+        self.assertTrue(np.allclose(
+            ssp.transmission.value,
+            ssp.gas_transmission_grid[0].value))
+
+        ssp.log_u = 0.0 << u.dex(u.dimensionless_unscaled)
+        self.assertTrue(np.allclose(
+            ssp.gas_L_lambda.value,
+            ssp.gas_L_lambda_grid[-1].value))
+        self.assertTrue(np.allclose(
+            ssp.transmission.value,
+            ssp.gas_transmission_grid[-1].value))
+
+    def test_cloudy_mixin_interpolate_with_gas_keeps_components_aligned(self):
+        ssp = make_dummy_cloudy_ssp(include_gas=True)
+
+        new_wl = np.array([150, 300, 600]) << u.AA
+        ssp.interpolate_sed(new_wl, verbose=False)
+
+        self.assertEqual(ssp.wavelength.size, new_wl.size)
+        self.assertEqual(ssp.stellar_L_lambda.shape[-1], new_wl.size)
+        self.assertEqual(ssp.gas_L_lambda_grid.shape[-1], new_wl.size)
+        self.assertEqual(ssp.gas_transmission_grid.shape[-1], new_wl.size)
+        self.assertEqual(ssp.gas_L_lambda.shape[-1], new_wl.size)
+        self.assertEqual(ssp.transmission.shape[-1], new_wl.size)
+        self.assertEqual(ssp.L_lambda.shape[-1], new_wl.size)
+
+    def test_cloudy_mixin_rejects_unsorted_log_u_array(self):
+        ssp = make_dummy_cloudy_ssp(include_gas=False)
+
+        with self.assertRaises(ValueError):
+            ssp.log_u_array = np.array([-1.0, -3.0]) << u.dex(u.dimensionless_unscaled)
+
+    def test_cloudy_mixin_rejects_malformed_gas_cube_shape(self):
+        ssp = make_dummy_cloudy_ssp(include_gas=False)
+        ssp.log_u_array = np.array([-3.0, -1.0]) << u.dex(u.dimensionless_unscaled)
+
+        with self.assertRaises(ValueError):
+            ssp.gas_L_lambda_grid = np.ones((2, 3, 2, 5)) << (u.Lsun / u.AA / u.Msun)
+
+        with self.assertRaises(ValueError):
+            ssp.gas_transmission_grid = np.ones((2, 2, 3, 5)) << u.dimensionless_unscaled
+
+    def test_cloudy_mixin_transmission_uses_pointwise_interpolation(self):
+        ssp = make_dummy_cloudy_ssp(include_gas=False)
+        ssp.log_u_array = np.array([-2.0]) << u.dex(u.dimensionless_unscaled)
+        ssp.gas_L_lambda_grid = np.ones((1, 2, 2, 5)) << (u.Lsun / u.AA / u.Msun)
+        transmission = np.array([0.0, 1.0, 0.0, 1.0, 0.0])
+        ssp.gas_transmission_grid = np.broadcast_to(transmission, (1, 2, 2, 5)) << u.dimensionless_unscaled
+        ssp.log_u = -2.0 << u.dex(u.dimensionless_unscaled)
+
+        new_wl = np.array([150, 300, 600]) << u.AA
+        ssp.interpolate_sed(new_wl, verbose=False)
+
+        expected = np.interp(new_wl.value, np.array([100, 200, 400, 800, 1600]), transmission)
+        self.assertTrue(np.allclose(ssp.transmission[0, 0].value, expected))
 
     def test_get_specific_mass_lum_ratio(self):
         ssp = make_dummy_ssp()
